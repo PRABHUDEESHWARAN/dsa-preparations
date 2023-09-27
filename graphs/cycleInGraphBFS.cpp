4c337c934ec6b39fdb012ecc2fe76f2c2c41c9af
Prabhudeeshwaran <prabhudeeshwaranganesh@gmail.com>
2023-09-28 00:03:29 +0530

    cycles in graph bfs

--- graphs/cycleInGraphBFS.cpp
+++ graphs/cycleInGraphBFS.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 /*
 Detect cycles in Graph using bfs: given a graph return true if the graph contains a cycle, the graph may have multiple components.
+    -> here we are given a graph and we need to find if cycle exist in it using bfs.
+    -> Therefore while doing bfs, we know that we will go level wise, and the idea is that
+        -> from root node we will split into two paths in the next level 
+        eg:  1      so, when we encounter a node which is already visited and it is not our parent then how can it be visited ?
+            2 3      -> it is becoz another path has already visited that node by the current path reached that node therefore 
+          4 5 6 7       a cycle exist in the graph.
 
-
+        -> to keep track of the parent and the current node we use pair in the queue.
 
 */
 class Solution
