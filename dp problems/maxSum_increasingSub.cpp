6884fb75a67b6702b1ca72bfce4a47e6db050b63
Prabhudeeshwaran <prabhudeeshwaranganesh@gmail.com>
2023-10-23 14:39:41 +0530

    maxSum increasingSubseq

---
+++ dp problems/maxSum_increasingSub.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    // Your code goes here
+	    int maxSum=arr[0];
+	    vector<int> dp;
+	    for(int i=0;i<n;i++)
+	    {
+	        dp.push_back(arr[i]);
+	    }
+	    
+	    for(int i=1;i<n;i++)
+	    {
+	        for(int j=0;j<i;j++)
+	        {
+	            if(arr[i]>arr[j] && dp[j]+arr[i]>dp[i])
+	            {
+	                dp[i]=dp[j]+arr[i];
+	            }
+	        }
+	        //update max value
+	        maxSum=max(maxSum,dp[i]);
+	    }
+	    return maxSum;
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
